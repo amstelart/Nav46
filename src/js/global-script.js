5c86950ee84aed8fc4e5c9a3c9b5e9bdc87bfeef
Артем <amstelart@MacBook-Pro-Artem.local>
2018-02-16 18:27:41 +0300

    fix js бага якорных ссылок

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -42,7 +42,7 @@ $( document ).ready(function() {
   });
 
   // Select all links with hashes
-  $('a[href*="#"]')
+  $('a.anchor[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')

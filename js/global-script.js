9a02f66d2ff42e2245dfb7e85cac5ba7d69675bc
Артем <amstelart@MacBook-Pro-Artem.local>
2018-03-07 20:00:48 +0300

    Update 2018-03-07T17:00:44.731Z

--- js/global-script.js
+++ js/global-script.js
@@ -11,6 +11,16 @@ $( document ).ready(function() {
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 2px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 2px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
   });
 
+  $(".page-slider").owlCarousel({
+    items: 1,
+    nav: false,
+    autoHeight: true,
+    loop: true,
+    center: true,
+    dots: true,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 2px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 2px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
+  });
+
   var owl = $('.gallery-carousel');
   owl.owlCarousel({
       loop: true,
@@ -81,9 +91,43 @@ $( document ).ready(function() {
 });
 
 // Изоляция без jQuery
-// (function(){
-//   // code
-// }());
+if(window.matchMedia('(min-width: 991px)').matches)
+{
+  // do functionality on screens smaller than 768px
+  (function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
+  var a = document.querySelector('.page-cols__l-col'), b = null;  // селектор блока, который нужно закрепить
+  window.addEventListener('scroll', Ascroll, false);
+  document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+  function Ascroll() {
+    if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
+      var Sa = getComputedStyle(a, ''), s = '';
+      for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
+        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+          s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+        }
+      }
+      b = document.createElement('div');  // создать потомка
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
+      a.style.padding = '0';
+      a.style.border = '0';  // если элементу присвоен padding или border
+    }
+    if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+      b.className = 'sidebar-nav--sticky';
+    } else {
+      b.className = '';
+    }
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false);  // если изменить размер окна браузера, измениться ширина элемента
+  }
+  })();
+}
 
 // На проекте нет jQuery, но хочется $( document ).ready...
 // function ready(fn) {

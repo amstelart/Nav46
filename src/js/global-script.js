7b75a4f1bca63d00987ff7b07ce8024b46e852a5
Артем <amstelart@MacBook-Pro-Artem.local>
2018-03-10 02:57:59 +0300

    переделал сайдбар меню

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -88,6 +88,43 @@ $( document ).ready(function() {
       }
     });
 
+    var menu_selector = ".sidebar-nav";
+
+    function onScroll() {
+      var scroll_top = $(document).scrollTop();
+      $(menu_selector + " a").each(function() {
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+          $(menu_selector + " a.active").removeClass("active");
+          $(this).addClass("active");
+        } else {
+          $(this).removeClass("active");
+        }
+      });
+    }
+
+
+    $(document).on("scroll", onScroll);
+
+    $("a[href^=#]").click(function(e) {
+      e.preventDefault();
+
+      $(document).off("scroll");
+      $(menu_selector + " a.active").removeClass("active");
+      $(this).addClass("active");
+      var hash = $(this).attr("href");
+      var target = $(hash);
+
+      $("html, body").animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        window.location.hash = hash;
+        $(document).on("scroll", onScroll);
+      });
+
+    });
+
 });
 
 // Изоляция без jQuery

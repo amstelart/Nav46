8d2f33c09183c0ff05f0dab5155fa88a85e31455
Артем <amstelart@MacBook-Pro-Artem.local>
2018-03-10 17:32:15 +0300

    Update 2018-03-10T14:32:13.692Z

--- js/global-script.js
+++ js/global-script.js
@@ -95,7 +95,7 @@ $( document ).ready(function() {
       $(menu_selector + " a").each(function() {
         var hash = $(this).attr("href");
         var target = $(hash);
-        if (target.position().top <= scroll_top && target.position().top + target.outerHeight(true) > scroll_top) {
+        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
           $(menu_selector + " a.active").removeClass("active");
           $(this).addClass("active");
         } else {
